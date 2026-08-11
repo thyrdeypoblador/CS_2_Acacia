7b3b22440616cb6d174147eaf5db657e9f1fc217
thyrdeypoblador <tajbpoblador31@src.pshs.edu.ph>
2026-08-11 09:09:52 +0800

    Add files via upload

---
+++ Q1_Acacia_FA1_Poblador.py
@@ -0,0 +1,19 @@
+import math
+x1 = float(input("Enter x1 value"))
+x2 = float(input("Enter x2 value"))
+y1 = float(input("Enter y1 value"))
+y2 = float(input("Enter y2 value"))
+
+x = x2 - x1
+y = y2 - y1
+xsqrt = math.pow(x, 2)
+ysqrt = math.pow(y, 2)
+d = math.sqrt(xsqrt + ysqrt)
+
+print("The distance between the two points is", d)
+
+#Reflection prompts
+#Why is using a library more practical than writing all calculations from scratch? Explain briefly using your activity as an example.
+#To make the process easier and to make the code readable. In our activity, we used libraries to calculate the distance between two points in a Cartesian Plane using pow() and sqrt(), without the libraries, your code would be 10x bigger, and it made adding, subtracting, etc, different amounts much simpler. Would be better if you calculated on paper instead.
+
+
